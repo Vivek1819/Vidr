6e13eba3f3b0fb97e6042a94966e55e33f410465
lorentzfroce <154860621+lorentzfroce@users.noreply.github.com>
2024-06-05 22:22:15 +0530

    completed comment controllers

--- routes/users.js
+++ routes/users.js
@@ -20,9 +20,9 @@ router.put("/sub/:id",verifyToken,subscribe)
 router.put("/unsub/:id",verifyToken,unsubscribe)
 
 //like a video
-router.put("/like/:id",verifyToken,like)
+router.put("/like/:videoId",verifyToken,like)
 
 //dislike a video
-router.put("/dislike/:id",verifyToken,dislike)
+router.put("/dislike/:videoId",verifyToken,dislike)
 
 export default router
